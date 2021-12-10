512c6b310e82e2855093d5ccb6555d88d49dfbcf
Farrukh <farrukh.agh.edu@gmail.com>
2021-12-10 14:26:29 +0100

    task 25 done

--- index.js
+++ index.js
@@ -18,12 +18,14 @@
 
 
 //         21) Given variables x = "John" and y = "Doe", write on the console "John <> Doe"
+console.log("\nTask 21\n");
 let x = "John"
 let y = "Doe"
 console.log(x + "<>" + y);
 
 
 ///////////------22) Create an object with properties such name, surname, email---------- ////////////
+console.log("\nTask 22\n");
 let myDetails = {
     name: "Farrukh",
     surname: "Abduganiev",
@@ -34,9 +36,18 @@ console.log(myDetails);
 
 
 //         23) Delete the email property from the previously created object //////////
+console.log("\nTask 23\n");
 let deleteEmail = delete myDetails.email
 console.log(myDetails);
 
 
 //         24) Create an array with 10 strings in it   //////////
 let arr = ["Macbook", "Ipad","Iphone", "Iwatch", "Airpods","six","seven","eight","nine","ten"]
+
+
+//         25) Print in the console every string from the previous array//////
+console.log("\nTask 25\n");
+console.log(arr);
+
+//         26) Create an array with 100 random numbers in it/////
+console.log("\nTask 26\n");
